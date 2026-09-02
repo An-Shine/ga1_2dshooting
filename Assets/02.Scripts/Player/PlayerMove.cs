34fd1f27d8b48a8ff3733dcc2499e593714959cc
An-Shine <akh1012@naver.com>
2026-09-02 12:57:41 +0900

    [Feat] 플레이어 속력 조정
    Q 를 눌러서 플레이어 속력 증가
    
    E를 눌러서 플레이어 속력 감소

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -6,10 +6,12 @@ public class PlayerMove : MonoBehaviour
     // 목적 : 키보드 입력에 따라서 플레이어 이동 처리를 하고싶다
     
     // 매직넘버 방지 : 보는사람에 따라 의미가 달라질 수 있는 숫자 값을 매직넘버 라고함
-    public float Speed;
-    public float LimitTopY = -0.6f;
-    public float LimitBottomY = -4.5f;
-    public float LimitX = 3.0f;
+    public float speed;
+    public float limitTopY = -0.6f;
+    public float limitBottomY = -4.5f;
+    public float limitX = 3.0f;
+    public float speedCount = 1.0f;
+    public float speedLimit = 1.0f;
     
     // 매 프레임마다 실행된다
     // 초당 프레임 실행 횟수 : 별다른 설정이 없을경우 가능한 많이
@@ -30,8 +32,8 @@ public class PlayerMove : MonoBehaviour
         // 3. 방향과 속력에 따라 이동한다
         // 속도 : 방향 * 속력
 
-        Vector2 normalizedspeed = (direction * Speed).normalized; // 벡터의 길이를 1로 만들어주는것 -> 방향만 유지한다
-        transform.Translate(direction * Speed * Time.deltaTime);
+        Vector2 normalizedspeed = (direction * speed).normalized; // 벡터의 길이를 1로 만들어주는것 -> 방향만 유지한다
+        transform.Translate(direction * speed * Time.deltaTime);
             
         //deltaTime : 이전 프레임으로부터 지금 프레임까지 시간이 얼마나 지났는지 ms 단위로 반환
         
@@ -39,38 +41,38 @@ public class PlayerMove : MonoBehaviour
         //transform.position += (Vector3)direction * Speed * Time.deltaTime;
         
         // 실습과제 1번
-        if (transform.position.y > LimitTopY)
+        if (transform.position.y > limitTopY)
         {
-            transform.position = new Vector2(transform.position.x, LimitTopY);
+            transform.position = new Vector2(transform.position.x, limitTopY);
         }
-        else if (transform.position.y < LimitBottomY)
+        else if (transform.position.y < limitBottomY)
         {
-            transform.position = new Vector2(transform.position.x, LimitBottomY);
+            transform.position = new Vector2(transform.position.x, limitBottomY);
         }
         
         // 실습과제 2번
-        if (transform.position.x > LimitX)
+        if (transform.position.x > limitX)
         {
-            transform.position = new Vector2(-LimitX, transform.position.y);
+            transform.position = new Vector2(-limitX, transform.position.y);
         }
         
-        else if (transform.position.x < -LimitX)
+        else if (transform.position.x < -limitX)
         {
-            transform.position = new Vector2(LimitX, transform.position.y);
+            transform.position = new Vector2(limitX, transform.position.y);
         }
 
         if (Input.GetKey(KeyCode.E))
         {
-            Speed--;
-            if (Speed <= 0.1f)
+            speed -= speedCount;
+            if (speed <= speedLimit)
             {
-                Speed = 0.1f; // 더이상 떨어지지 않게끔 , 하지만 기획의도에 따라 0으로 가게끔 수정가능
+                speed = speedLimit; // 더이상 떨어지지 않게끔 , 하지만 기획의도에 따라 0으로 가게끔 수정가능
             }
         }
 
         if (Input.GetKey(KeyCode.Q))
         {
-            Speed++;
+            speed+= speedCount;
         }
         
         
