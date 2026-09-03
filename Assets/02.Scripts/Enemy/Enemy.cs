41aae6e0fa031418f74c9872b0d388b56d1712f6
An-Shine <akh1012@naver.com>
2026-09-03 13:53:50 +0900

    feat: 보조총알 데미지 수정
    데미지 수정을 하긴했는데...

--- Assets/02.Scripts/Enemy/Enemy.cs
+++ Assets/02.Scripts/Enemy/Enemy.cs
@@ -3,11 +3,19 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float Speed;
-    public float Health = 100;
+    public float Health;
+    public GameObject EnemyPrefab;
+    public Transform EnemySpawnPoint;
 
     private void Update()
     {
         Vector2 direction = Vector2.down;
         transform.Translate(direction * (Speed * Time.deltaTime));
+    }
+
+    private void SpawnEnemy()
+    {
+        GameObject enemy = Instantiate(EnemyPrefab);
+        enemy.transform.position = EnemySpawnPoint.position;
     }
 }
