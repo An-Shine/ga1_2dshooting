679eede6bbd742355e398351a46eead0a03292d8
An-Shine <akh1012@naver.com>
2026-09-03 10:31:12 +0900

    feat: pre-commit 코딩 컨벤션 설정 추가
    pre-commit 코딩 컨벤션 설정 추가

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -2,14 +2,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerFire : MonoBehaviour
-{   
-    
+{
     // 목표 : 스페이스바를 누를때 마다 총알을 생성해서 발사하기
     // 필요 속성 : 총알 프리펩, 생성위치(발사지점)
 
     public GameObject BulletPrefab;
     public GameObject SubBulletPrefabs;
     public Transform[] FirePoints;
+
     public Transform[] SubFirePoints;
     //public Transform LeftFirePoint;
     //public Transform RightFirePoint;
@@ -23,25 +23,27 @@ public class PlayerFire : MonoBehaviour
     {
         CurrentCooldown = FireCooldown;
     }
+
     private void Update()
     {
         // 쿨타임 적용
         CurrentCooldown -= Time.deltaTime;
-        
-        if (CurrentCooldown <= 0 && Input.GetKeyDown(KeyCode.Space)&& isAutoFire!=true)
+
+        if (CurrentCooldown <= 0 && Input.GetKeyDown(KeyCode.Space) && isAutoFire != true)
         {
             Fire();
-            
+
             // 쿨타이머 초기화 (중요)
             CurrentCooldown = FireCooldown;
         }
-        
-        
+
+
         // 1번 눌러서 자동발사 모드 설정 , 다시 1번 누르면 자동모드 OFF
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             isAutoFire = !isAutoFire;
         }
+
         if (isAutoFire == true && CurrentCooldown <= 0)
         {
             AutoFire();
@@ -52,7 +54,7 @@ public class PlayerFire : MonoBehaviour
     {
         // 1. 스페이스바를 누르면 
         if (Input.GetKeyDown(KeyCode.Space))
-        {   
+        {
             // 2. 총알 프리펩을 생성한다
             /*
             GameObject leftBullet = Instantiate(BulletPrefab);
@@ -60,7 +62,7 @@ public class PlayerFire : MonoBehaviour
             GameObject rightBullet = Instantiate(BulletPrefab);
             rightBullet.transform.position = RightFirePoint.position;
             */
-            
+
             foreach (Transform firePoint in FirePoints)
             {
                 GameObject bullet = Instantiate(BulletPrefab);
@@ -80,13 +82,15 @@ public class PlayerFire : MonoBehaviour
         foreach (Transform firePoint in FirePoints)
         {
             GameObject bullet = Instantiate(BulletPrefab);
-            bullet.transform.position = firePoint.position;    
+            bullet.transform.position = firePoint.position;
         }
+
         foreach (Transform firePoint in SubFirePoints)
         {
             GameObject bullet = Instantiate(SubBulletPrefabs);
             bullet.transform.position = firePoint.position;
         }
+
         CurrentCooldown = FireCooldown;
     }
 }
--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem.Layouts;
 public class PlayerMove : MonoBehaviour
 {
     // 목적 : 키보드 입력에 따라서 플레이어 이동 처리를 하고싶다
-    
+
     // 매직넘버 방지 : 보는사람에 따라 의미가 달라질 수 있는 숫자 값을 매직넘버 라고함
     public float speed;
     public float limitTopY = -0.6f;
@@ -12,7 +12,8 @@ public class PlayerMove : MonoBehaviour
     public float limitX = 3.0f;
     public float speedCount = 1.0f;
     public float speedLimit = 1.0f;
-    
+
+
     // 매 프레임마다 실행된다
     // 초당 프레임 실행 횟수 : 별다른 설정이 없을경우 가능한 많이
     private void Update()
@@ -24,28 +25,28 @@ public class PlayerMove : MonoBehaviour
     private void Move()
     {
         // 1. 키보드 입력을 받는다 (GetAxis / GetAxisRaw)
-        float h = Input.GetAxisRaw("Horizontal");  // 키보드 왼/오른쪽 입력 상태에 따라 -1f ~ 0 ~ 1f 를 반환
-        float v = Input.GetAxisRaw("Vertical");    // 키보드 위/아래 입력 상태에 따라 -1f ~ 0 ~ 1f 를 반환
-        
-        
+        float h = Input.GetAxisRaw("Horizontal"); // 키보드 왼/오른쪽 입력 상태에 따라 -1f ~ 0 ~ 1f 를 반환
+        float v = Input.GetAxisRaw("Vertical"); // 키보드 위/아래 입력 상태에 따라 -1f ~ 0 ~ 1f 를 반환
+
+
         Debug.Log($"h:{h}, v:{v}");
-        
+
         // 2. 키보드 입력에 따라 방향을 구한다
         // 게임에는 벡터 라는 타입이 있다. 벡터는 크기와 방향을 의미한다
-        
+
         Vector2 direction = new Vector2(h, v);
-        
+
         // 3. 방향과 속력에 따라 이동한다
         // 속도 : 방향 * 속력
 
         Vector2 normalizedSpeed = (direction * speed).normalized; // 벡터의 길이를 1로 만들어주는것 -> 방향만 유지한다
         transform.Translate(direction * speed * Time.deltaTime);
-            
+
         //deltaTime : 이전 프레임으로부터 지금 프레임까지 시간이 얼마나 지났는지 ms 단위로 반환
-        
+
         // 새로운 위치 : 현재위치 + (방향 * 속력 * 시간)
         //transform.position += (Vector3)direction * Speed * Time.deltaTime;
-        
+
         // 실습과제 1번
         if (transform.position.y > limitTopY)
         {
@@ -55,13 +56,13 @@ public class PlayerMove : MonoBehaviour
         {
             transform.position = new Vector2(transform.position.x, limitBottomY);
         }
-        
+
         // 실습과제 2번
         if (transform.position.x > limitX)
         {
             transform.position = new Vector2(-limitX, transform.position.y);
         }
-        
+
         else if (transform.position.x < -limitX)
         {
             transform.position = new Vector2(limitX, transform.position.y);
@@ -81,7 +82,7 @@ public class PlayerMove : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            speed+= speedCount;
+            speed += speedCount;
         }
     }
 }
