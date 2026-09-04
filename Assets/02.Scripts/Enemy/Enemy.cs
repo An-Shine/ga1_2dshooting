a6c3bd93969875e7da9c37066b89ab2ec8703d7a
An-Shine <akh1012@naver.com>
2026-09-04 13:46:29 +0900

    feat: Enemy 와 Player 간의 충돌데미지 구현
    Enemy 와 Player 간의 충돌데미지 구현

--- Assets/02.Scripts/Enemy/Enemy.cs
+++ Assets/02.Scripts/Enemy/Enemy.cs
@@ -4,6 +4,7 @@ public abstract class Enemy : MonoBehaviour
 {
     [SerializeField] private int _health;
     [SerializeField] protected float _moveSpeed;
+    [SerializeField] private int _collisionDamage;
 
 
     private void Update()
@@ -21,6 +22,16 @@ public abstract class Enemy : MonoBehaviour
         {
             // 충돌한 대상 파괴 (Enemy)
             Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerHealth playerHealth = collision.gameObject.GetComponent<PlayerHealth>();
+            Destroy(gameObject);
+            playerHealth.TakeDamage(_collisionDamage);
         }
     }
 }
---
+++ Assets/02.Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float _maxHealth;
+    public float _currentHealth;
+
+    public void Start()
+    {
+        _maxHealth = 100f;
+        _currentHealth = _maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        _currentHealth -= damage;
+        if (_currentHealth <= 0f)
+        {
+            _currentHealth = 0f;
+            Destroy(gameObject);
+        }
+    }
+}
